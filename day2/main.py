1318c768a775ab1143be8da7146186955187d668
Luke <luke.stephens@trintel.co.za>
2023-12-04 17:38:49 +0200

    it works

--- day2/main.py
+++ day2/main.py
@@ -4,29 +4,26 @@ import re
 def problem1():
     with open('data.txt', 'r') as file:
         total_possible = 0
-        for line in file:
-            data_arr = re.split("[;:]", line.strip())
+        for game in file:
+            data_arr = re.split("[;:]", game.strip())
             id = int(''.join(filter(str.isdigit, data_arr[0])))
-            possible = False
-            for item in data_arr:
-                blue = 0
-                red = 0
-                green = 0
-                item = item.split(",")
-                for string in item:
+            blue = 0
+            red = 0
+            green = 0
+            for game_set in data_arr:
+                game_set = game_set.split(",")
+                for string in game_set:
                     val = int(''.join(filter(str.isdigit, string)))
                     if "blue" in string:
-                        blue += val
+                        blue = val if val > blue else blue
                     if "red" in string:
-                        red += val
+                        red = val if val > red else red
                     if "green" in string:
-                        green += val
-                enough_balls = 14 >= blue > 0 and 12 >= red > 0 and 13 >= green > 0
-                total_score = blue + red + green
-                possible = enough_balls and 39 >= total_score > 0
+                        green = val if val > green else green
+            possible = 14 >= blue > 0 and 12 >= red > 0 and 13 >= green > 0
             if possible:
                 total_possible += id
-                print(f"ID:{id}", blue, red, green, f"S:{total_score}", f"P:{possible}")
+            print(f"ID:{id}", blue, red, green, f"P:{possible}")
     print(f"Total Possible: {total_possible}")
 
 
